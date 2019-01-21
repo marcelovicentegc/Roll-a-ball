7c75142b5c838883353336e342aad98d77fb1d95
Marcelo Cardoso <marcelovicentegc@gmail.com>
2019-01-21 18:32:27 -0300

    First commit

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public GameObject player;
+    public Vector3 offset;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = transform.position - player.transform.position;    
+    }
+
+    // LateUpdate is called once per frame after everything that has to run
+    void LateUpdate()
+    {
+        transform.position = player.transform.position + offset;
+    }
+}
